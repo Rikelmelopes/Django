faadcac9b1c3ccab20975aefacb76618c20a89a0
Rikelmelopes <rikelme.lopes777@gmail.com>
2023-06-11 20:50:14 -0300

    Adicionando marcas

--- cars/admin.py
+++ cars/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Car, Brand
 # Register your models here.
 
 class CarAdmin (admin.ModelAdmin):
     list_display = ('model','brand','factore_year','model_year','value')
     search_fields = ('model','brand')
     
+class BrandAdmin (admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    
+admin.site.register(Brand, BrandAdmin)
 admin.site.register(Car, CarAdmin)
+
+    
