ee85615da52be330abe4bcf3722fcea38e836b7e
Rikelmelopes <rikelme.lopes777@gmail.com>
2023-06-29 20:32:15 -0300

    Ordenando itens de uma tabela com order_by

--- cars/views.py
+++ cars/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from cars.models import Car
 
 def cars_view(request):
-    cars = Car.objects.filter(model='Camaro')
-
+    cars = Car.objects.all().order_by('model')
+    search = request.GET.get('search')
     
+    if search:
+        cars = Car.objects.filter(model='Camaro')
+        
     
     return render(request,
                   'cars.html',
