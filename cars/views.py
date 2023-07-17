5e90b42ace4fe64e585085bb10167e71a47df55f
Rikelmelopes <rikelme.lopes777@gmail.com>
2023-07-17 18:58:48 -0300

    Usando novos metodos de ListView

--- cars/views.py
+++ cars/views.py
@@ -1,12 +1,14 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render ,redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
-
+from django.views.generic import ListView
 
 
     
-class CarsView(View):
+'''class CarsView(View):
     def get(self,request):
         cars = Car.objects.all().order_by('model')
         search = request.GET.get('search')
@@ -17,7 +19,20 @@ class CarsView(View):
         return render(request,
                   'cars.html',
                   {'cars':cars }
-                  )
+                  )'''
+    
+class CarsListView(ListView):
+    model = Car 
+    template_name = 'cars.html'
+    context_object_name = 'cars'   
+    
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model') 
+        search = self.request.GET.get('search')
+        if search:
+            cars = cars.filter(model__icontains=search)
+        return cars 
+      
     
 class NewCarView(View):
     def get(self,request):
