a4841468a6b13f41100082bc270a4f998505da3d
Rikelmelopes <rikelme.lopes777@gmail.com>
2023-07-25 18:55:10 -0300

    fazendo melhorias

---
+++ openai_api/client.py
@@ -0,0 +1,14 @@
+#import openai
+
+#def get_car_ai_bio(model,brand,year):
+   # prompt = '''
+    #Me mostre uma descrição de venda para o carro {} {} {} em apenas 250 caracteres.Fale apenas coisas especificas desse modelo.
+    #'''
+   # openai.api_key = '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'
+    #prompt = prompt.format(brand,model,year)
+    #response = openai.Completion.create(
+       # model = 'text-davinci-003',
+       # prompt= prompt,
+       # max_tokens=1000,
+    #)
+    #return response['choices'][0]['text']
